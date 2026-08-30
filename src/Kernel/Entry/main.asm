1ff5f616950edb907a62b2e6387ce57495cffcd7
Patrick VDM <Patrick.VDM001@gmail.com>
2026-08-30 21:09:27 +1200

    fix code to add stack for C code to work

--- src/Kernel/Entry/main.asm
+++ src/Kernel/Entry/main.asm
@@ -6,5 +6,11 @@ bits 32
 xeravend_start:
     ; print 'OK'
     ;mov dword [0xb8000], 0x2f4b2f4f
+    mov esp, stack_top
     call xkmain
     hlt
+
+section .bss
+stack_bottom:
+    resb 4096 * 4
+stack_top:
